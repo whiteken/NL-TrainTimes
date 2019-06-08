9e4d446c80b4dc9f6a71a3f8adbdcccb4284f293
White <Kenneth.White@ing.com>
2019-06-08 15:46:55 +0200

    removing psscriptanalyzer tests, will replace with marketplace add-in

--- Tests/NSTrainTime.tests.ps1
+++ Tests/NSTrainTime.tests.ps1
@@ -96,37 +96,5 @@ InModuleScope NSTrainTime {
                 }
             }
         }
-
-        Context 'Checking files to test exist and Invoke-ScriptAnalyzer is available'{
-            
-            It 'Checking files exist to test'{
-                $moduleFiles = Get-ChildItem $global:BuildFolder\* -Include *.psd1, *.psm1, *.ps1 -Exclude *.tests.ps1 -Recurse
-                $moduleFiles.count | Should Not Be 0
-            }          
-            
-            It 'Checking Invoke-ScriptAnalyzer exists' {
-                { Get-Command Invoke-ScriptAnalyzer -ErrorAction Stop } | Should Not Throw
-            }
-        }
-
-        $scriptAnalyzerRules = Get-ScriptAnalyzerRule
-        $moduleFiles = Get-ChildItem $global:BuildFolder\* -Include *.psd1, *.psm1, *.ps1 -Exclude *.tests.ps1, *.txt -Recurse
-
-        foreach ($file in $moduleFiles){
-            
-            switch -wildCard ($file){ 
-                '*.psm1' {$testType = 'Module'} 
-                '*.ps1'  {$testType = 'Script'} 
-                '*.psd1' {$testType = 'Manifest'} 
-            }
-    
-            Context "Checking $($file.name) - meets $testType PSScriptAnalyzer Rule"{
-                foreach ($scriptAnalyzerRule in $scriptAnalyzerRules){
-                    It "Script Analyzer Rule $scriptAnalyzerRule"{
-                        (Invoke-ScriptAnalyzer -Path $file -IncludeRule $scriptAnalyzerRule).count | Should Be 0
-                    }
-                }
-            }
-        }
     }
 }
