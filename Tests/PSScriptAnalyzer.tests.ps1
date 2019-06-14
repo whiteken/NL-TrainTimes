26ef348f61e4fd472a007f6bbe9f05575f829e81
White <Kenneth.White@ing.com>
2019-06-14 19:01:00 +0200

    Adding back script analyzer tests

---
+++ Tests/PSScriptAnalyzer.tests.ps1
@@ -0,0 +1,48 @@
+param(
+    #This is passed in from AzureDevOps during the build
+    #otherwise look to the parent folder from where the Tests are located
+    [parameter()][string]$BuildFolder='..'
+)
+
+$global:BuildFolder = (Split-Path -Parent $MyInvocation.MyCommand.Path) -replace '\\Tests'
+
+Get-Module NSTrainTime | Remove-Module
+Import-Module "$BuildFolder\NSTrainTime.psd1" –ArgumentList $true -Force -ErrorAction Stop -Verbose -Scope Local
+
+InModuleScope NSTrainTime {
+    
+    Describe 'NSTrainTime individual pester tests' {
+    
+        Context 'Checking files to test exist and Invoke-ScriptAnalyzer is available'{
+                
+            It 'Checking files exist to test'{
+                $moduleFiles = Get-ChildItem $global:BuildFolder\* -Include *.psd1, *.psm1, *.ps1 -Exclude *.tests.ps1 -Recurse
+                $moduleFiles.count | Should Not Be 0
+            }          
+            
+            It 'Checking Invoke-ScriptAnalyzer exists' {
+                { Get-Command Invoke-ScriptAnalyzer -ErrorAction Stop } | Should Not Throw
+            }
+
+            $scriptAnalyzerRules = Get-ScriptAnalyzerRule
+            $moduleFiles = Get-ChildItem $global:BuildFolder\* -Include *.psd1, *.psm1, *.ps1 -Exclude *.tests.ps1, *.txt -Recurse
+
+            foreach ($file in $moduleFiles){
+
+                switch -wildCard ($file){ 
+                    '*.psm1' {$testType = 'Module'} 
+                    '*.ps1'  {$testType = 'Script'} 
+                    '*.psd1' {$testType = 'Manifest'} 
+                }
+
+                Context "Checking $($file.name) - meets $testType PSScriptAnalyzer Rule"{
+                    foreach ($scriptAnalyzerRule in $scriptAnalyzerRules){
+                        It "Script Analyzer Rule $scriptAnalyzerRule"{
+                            (Invoke-ScriptAnalyzer -Path $file -IncludeRule $scriptAnalyzerRule).count | Should Be 0
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
