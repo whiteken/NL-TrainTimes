7651c13d913b0d7e7636d1eedb269279658956ec
Kenny White <kennywhite1@gmail.com>
2021-03-07 22:58:01 +0100

    Update Get-NSTrainJourney.ps1

--- AzureServerless/Get-NSTrainJourney.ps1
+++ AzureServerless/Get-NSTrainJourney.ps1
@@ -1,35 +1,55 @@
+using namespace System.Net
 
-$fromStation = 'Hilversum'
-$toStation = 'Amersfoort'
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+# Interact with query parameters or the body of the request.
+$fromStation = $Request.Query.fromStation
+if (-not $fromStation) {
+    $fromStation = ($Request.Body | ConvertFrom-Json).fromStation
+    Write-Host "From station is $fromStation"
+}
+
+$toStation = $Request.Query.toStation
+if (-not $toStation) {
+    $toStation = ($Request.Body | ConvertFrom-Json).toStation
+    Write-Host "To station is $toStation"
+}
+
 $uri = 'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/trips?fromStation={0}&toStation={1}&originWalk=false&originBike=false&originCar=false&destinationWalk=false&destinationBike=false&destinationCar=false&shorterChange=false&travelAssistance=false&searchForAccessibleTrip=false&localTrainsOnly=false&excludeHighSpeedTrains=false&excludeTrainsWithReservationRequired=false&yearCard=false&discount=NO_DISCOUNT&travelClass=2&polylines=false&passing=false&travelRequestType=DEFAULT' -f $fromStation, $toStation
 
+#Retrieve code from https://apiportal.ns.nl/signin?ReturnUrl=%2Fdeveloper
+#replace during AzureDevOps build from AZ keyvault
 $headers = @{
-    "Ocp-Apim-Subscription-Key" = ""
+    "Ocp-Apim-Subscription-Key" = "%%ns-subscription-key%%"
 }
 
 $raw = Invoke-RestMethod -Uri $URI -Headers $headers -Method Get
 
-$raw.trips | ForEach-Object {
+$body = $raw.trips | ForEach-Object {
     [PSCustomObject]@{
-        PlannedDuration = $_.plannedDurationInMinutes
-        ActualDuration = $_.actualDurationInMinutes
+        FromStation = $fromStation
+        ToStation = $toStation
+        Stops = $($_.legs.stops.name).count
+        DurationMins = $_.plannedDurationInMinutes
         DeparturePlatform = $_.legs.stops.plannedDepartureTrack[0]
         Status = $_.Status
         CrowdForecast = $_.crowdForecast
-        Type = $_.type
         Punctuality = $_.punctuality  
-        Stops = $($_.legs.stops.name).count
         DepartureTime = $_.legs.stops.plannedDepartureDateTime[0]
         ArrivalTime = $_.legs.stops.plannedArrivalDateTime[-1]
     }
 }
 
 
+Write-Host "Returned number of trips: $($raw.trips | Measure-Object | Select-Object -expandProperty Count)"
 
-$body = [PSCustomObject]@{
-    fromStation = 'Hilversum'
-    toStation = 'Amersfoort'
-} | ConvertTo-Json
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $body
 
-$uri = 'https://nstraintime2.azurewebsites.net/api/Get-NSTrainJourney'
-Invoke-RestMethod -uri $uri -Method Post -Body $body -Verbose | ft
+})
