f23c70a24373bd236015e4bf4dd24a38200797aa
Kenny White <kennywhite1@gmail.com>
2021-03-07 22:58:36 +0100

    Update Get-NSTrainStation.ps1

--- AzureServerless/Get-NSTrainStation.ps1
+++ AzureServerless/Get-NSTrainStation.ps1
@@ -1,26 +1,41 @@
+using namespace System.Net
 
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
 
-[uri]$URI = "https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations"
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
 
-$headers = @{
-    "Ocp-Apim-Subscription-Key" = ""
+# Interact with query parameters or the body of the request.
+$station = $Request.Query.Station
+if (-not $station) {
+    $station = ($Request.Body | ConvertFrom-Json).Station
+    Write-Host "Station is $station"
 }
 
-$Station = "Hilver"
+[uri]$uri = "https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations"
 
-$raw = Invoke-RestMethod -Uri $URI -Headers $headers -Method Get
+#Retrieve code from https://apiportal.ns.nl/signin?ReturnUrl=%2Fdeveloper
+#replace during AzureDevOps build from AZ keyvault
+$headers = @{
+    "Ocp-Apim-Subscription-Key" = "%%ns-subscription-key%%"
+}
 
-$raw.PayLoad | ForEach-Object {
+$raw = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
+
+$result = $raw.PayLoad | ForEach-Object {
     [PSCustomObject]@{
         UICCode = $_.UICCode
         ShortName = $_.Namen.kort 
         MiddleName = $_.Namen.middel 
         LongName = $_.Namen.lang
     }
-} | Where-Object {$_.LongName -like "*$Station*"} | ConvertTo-Json
+} | Sort-Object LongName
 
+$result = $result | Where-Object {$_.LongName -like "*$station*"}
 
-
-
-$body = [PSCustomObject]@{Station='RAI'} | ConvertTo-Json
-Invoke-RestMethod -uri https://nstraintime2.azurewebsites.net/api/Get-NSTrainStation -Method Post -Body $body
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $result
+})
